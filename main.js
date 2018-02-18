4cbc644234a2efe5ccaaad981579012f42c9fcde
Saima Hassan <saima.beti@gmail.com>
2018-02-18 05:20:22 +0000

    Funny website!

---
+++ main.js
@@ -0,0 +1,47 @@
+// EasyClick Sounds
+function sayThatWasEasy() { 
+    var thatWasEasy = new Audio("that_was_easy.mp3");
+    thatWasEasy.play();
+}
+
+$("#easy").on("click", sayThatWasEasy);
+$(document).keypress(delegateKeypress);
+
+//EasySpacebar Sounds
+function delegateKeypress(event) {
+    if (event.charCode == 32) {
+    $("#easy").trigger("click");
+ }
+}
+
+//HardButton Sounds
+var airhorn = new Audio();
+airhorn.src = "https://www.myinstants.com/media/sounds/mlg-airhorn.mp3";
+function PlayAirhornSound() {
+    airhorn.play();
+}
+
+//LOUDButton Action
+$(function(){
+    $("#loud").on({
+        mouseover:function(){
+            var left = parseFloat($(this).css("left"));
+            var top = parseFloat($(this).css("top"));
+            $(this).css("left", Number(left)+(Math.random()-0.5)*200);
+            $(this).css("top", top+(Math.random()-0.5)*200);
+            
+        }
+    });
+});
+
+//Unexpected Action
+function close_window() {
+  if (confirm("Do you want to see a funny video?")) {
+    close();
+  }
+}
+
+//WhoCares Sound
+function show_video(){
+            $("#video").css("display", "block");
+}
